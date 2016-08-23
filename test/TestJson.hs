0aa7ff42b320b2b598a6186545c13d766f46e300
Jost Berthold <jost.berthold@cba.com.au>
2016-08-23 17:18:02 +1000

    test for Json parsing

---
+++ test/TestJson.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module TestJson where
+
+import Distribution.TestSuite.QuickCheck
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+
+-- import           Data.Text(Text)
+-- import qualified Data.Text as T
+-- import qualified Data.Text.IO as T
+
+import Control.Monad.IO.Class
+import Data.Either
+
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as B
+import          System.FilePath
+
+import Text.Web.Diamond.Types
+
+tests :: IO [Test]
+tests = return [ wildJsonParsing ]
+
+wildJsonParsing :: Test
+wildJsonParsing = testGroup "parsing JSON collected in the wild"
+    [ testProperty "parse request bodies" $
+      resource "requestbodies.json" `canParseAs` (undefined :: CfPageBody)
+    , testProperty "parse responses" $
+      resource "responses.json" `canParseAs` (undefined :: CfResponse)
+    ]
+
+resource :: FilePath -> FilePath
+resource = ("test" </>) . ("resources" </>)
+
+-- Using a lame type argument trick: we won't actually use the thing
+canParseAs :: forall a . A.FromJSON a => FilePath -> a -> Property
+file `canParseAs` thing = monadicIO $
+          do json <- liftIO $ B.readFile file
+             let content :: Either String [a]
+                 content = A.eitherDecode json
+             assert (isRight content)
